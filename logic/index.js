665c762b4c30f5d80037944ef9f9d9cacc366f95
BernhardW <bernhard.woltron@gmail.com>
2024-02-15 17:54:03 +0100

    finished Landing Page, added Nav Scroll Logic

---
+++ logic/index.js
@@ -0,0 +1,14 @@
+"use strict";
+
+let navbar = document.getElementById("navbar");
+
+const dynClass = "scroll-style";
+const onScroll = () => {
+    if (window.scrollY > 0) {
+        navbar.classList.add(dynClass)
+    } else {
+        navbar.classList.remove(dynClass)
+    }
+}
+
+window.addEventListener("scroll", onScroll);
