74bbd8eefe3384e8a7e219ff321a74344ec435bc
BernhardW <bernhard.woltron@gmail.com>
2024-02-17 21:48:39 +0100

    responsive landing page hero finished

--- logic/main.js
+++ logic/main.js
@@ -1,17 +1,21 @@
 "use strict";
 
 let navbar = document.getElementById("navbar");
+let mobileNavbar = document.getElementById("mobile-nav");
 
 const dynClass = "scroll-style";
 const onScroll = () => {
     if (window.scrollY > 0) {
         navbar.classList.add(dynClass)
+        mobileNavbar.classList.add(dynClass)
     } else {
         navbar.classList.remove(dynClass)
+        mobileNavbar.classList.remove(dynClass)
     }
 }
 
 window.addEventListener("scroll", onScroll);
+
 
 
 
