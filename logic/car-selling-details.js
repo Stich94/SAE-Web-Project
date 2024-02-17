18171f42c8da8d4a0c4890bc3f2dc7f8bdafcca8
BernhardW <bernhard.woltron@gmail.com>
2024-02-17 16:16:45 +0100

    added car detail scroll logic

--- logic/car-selling-details.js
+++ logic/car-selling-details.js
@@ -1,4 +1,43 @@
 "use strict";
 
-let highlightElement = document.getElementById("scroll-to-highlights");
+let highlightButton = document.getElementById("scroll-to-highlights");
+let specsButton = document.getElementById("scroll-to-specs");
 
+
+let scrollTargetHighlights = document.getElementById("highlights-container");
+let scrollTargetCarSpecs = document.getElementById("car-specs-section");
+
+
+// add Event to click
+// highlightButton.addEventListener("click", scrollToHighlightsContainer);
+
+// ------- more generic func ------ to avoid code duplication -----
+highlightButton.addEventListener("click", (e) => {
+    scrollToTargetContainer(e,  scrollTargetHighlights);
+});
+
+specsButton.addEventListener("click", (e) => {
+    scrollToTargetContainer(e,  scrollTargetCarSpecs);
+});
+
+
+
+// ------------- Functions -------------
+function scrollToHighlightsContainer(e) {
+    e.preventDefault();
+
+    scrollTargetHighlights.scrollIntoView({
+        behavior: "smooth",
+        block: "start"
+    });
+}
+
+/// generic func to pass the target in, because logic is the same
+function scrollToTargetContainer(e, target) {
+    e.preventDefault();
+
+    target.scrollIntoView({
+        behavior: "smooth",
+        block: "start"
+    });
+}
